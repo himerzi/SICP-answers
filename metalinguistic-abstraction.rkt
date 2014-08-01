4b9a9056f278d0657808904c46834f6ac7f5f584
Michael Detmold <mdetmold@gmail.com>
2014-08-01 18:34:38 -0400

    Ex

---
+++ metalinguistic-abstraction.rkt
@@ -0,0 +1,19 @@
+;Ex 4.1
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+                                        ;LR
+
+(define (list-of-values-lr exps env)
+  (if (no-operands? exps)
+      '()
+      (let (left)
+        (let ((list-of-values (rest-operands exps) env))))) )
+
+(define (application? exp)
+  (tagged-list? exp 'call))
+
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))
