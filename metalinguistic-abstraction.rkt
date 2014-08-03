cfa1870f65a8d48430bdaa032aab81fbd83e2983
Michael Detmold <mdetmold@gmail.com>
2014-08-02 21:55:15 -0400

    And expression implemented. Nice!

--- metalinguistic-abstraction.rkt
+++ metalinguistic-abstraction.rkt
@@ -31,3 +31,29 @@
   (define (definition exp env) (eval-definition exp env))
   ( (get 'eval (car exp)) (cdr exp) env)
   (put 'eval 'define definition))
+
+;;Ex 4.4
+(define (and? exp) (tagged-list? exp 'and))
+(define (and-op1 exp) (car exp))
+(define (and-op2 exp) (cdr exp))
+(define (and->if exp env)
+  (eval (eval-and (cdr exp)) env))
+
+(define (eval-and exp)
+  (let ((first (and-op1 exp))
+        (rest (and-op2 exp)))
+    (if (null? rest)
+        (make-if first
+                 first
+                 'false)
+        (make-if first
+                 (eval-and rest)
+                 'false))))
+
+(define (or? exp)
+  (tagged-list? exp 'or))
+(define (or-op1 exp) (car exp))
+(define (or-op2 exp) (cdr exp))
+(define (or->if exp) (make-or (cdr exp)))
+(define (make-or exp)
+  )
